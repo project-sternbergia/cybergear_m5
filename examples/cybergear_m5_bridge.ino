5c555833d5fc9abe50deb3b344ca7dbeda6ead18
chikuta <takeshi.chiku@gmail.com>
2023-11-03 22:50:05 +0900

    Added ros2 logo for bridge example

--- examples/cybergear_m5_bridge.ino
+++ examples/cybergear_m5_bridge.ino
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <mcp_can.h>
 #include <M5Stack.h>
+#include "ros2_logo.hh"
 #include "cybergear_bridge.hh"
 #include "cybergear_controller.hh"
 
@@ -18,7 +19,6 @@ MCP_CAN CAN0(12);    // Set CS to pin 10
 
 // setup master can id and motor can id (default cybergear can id is 0x7F)
 uint8_t MASTER_CAN_ID = 0x00;
-
 uint8_t MOT_LEADER_CAN_ID = 0x7F;
 uint8_t MOT_FOLLOWER_CAN_ID = 0x7E;
 std::vector<uint8_t> motor_ids = {MOT_LEADER_CAN_ID, MOT_FOLLOWER_CAN_ID};
@@ -31,19 +31,12 @@ CybergearBridge bridge = CybergearBridge(&controller, &Serial);
 void setup()
 {
   M5.begin();
-  M5.Lcd.begin();             // 画面初期化
-  M5.Lcd.setRotation(1);      // 画面向き設定（0～3で設定、4～7は反転)※初期値は1
-  M5.Lcd.setTextWrap(false);  // 画面端での改行の有無（true:有り[初期値], false:無し）※print関数のみ有効
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.drawBitmap((M5.Lcd.width() - imgWidth)/2, (M5.Lcd.height() - imgHeight)/2, imgWidth, imgHeight, (uint16_t *)img);
 
   // init cybergear driver
   init_can();
   controller.init(motor_ids, MODE_CURRENT, &CAN0);
-
-  //launch task 0 loop
-  // xTaskCreatePinnedToCore(task0, "Task0", 4096, NULL, 1, NULL, 1);
-
-  // launch task 1 loop
-  // xTaskCreatePinnedToCore(task1, "Task1", 4096, NULL, 2, NULL, 1);
 }
 
 void loop()
@@ -52,7 +45,6 @@ void loop()
   M5.update();
   bridge.process_request_command();
   bridge.process_motor_response();
-  M5.Lcd.setCursor(0, 0);     // 表示座標を指定(x座標, y座標)
 }
 
 void init_can()
