8d671a98a57725aae320a382e2cfbdffec9c4ff2
chikuta <takeshi.chiku@gmail.com>
2024-05-08 20:05:36 +0900

    Added twai interface for can connection

---
+++ src/cybergear_can_interface_twai.cpp
@@ -0,0 +1,49 @@
+#include "cybergear_can_interface_twai.hh"
+
+// #include <M5Unified.h>
+
+#include "ESP32-TWAI-CAN.hpp"
+#include "cybergear_driver_utils.hh"
+
+CybergearCanInterfaceTwai::CybergearCanInterfaceTwai() : CybergearCanInterface() {}
+
+CybergearCanInterfaceTwai::~CybergearCanInterfaceTwai() {}
+
+bool CybergearCanInterfaceTwai::init(uint8_t rx_pin, uint8_t tx_pin)
+{
+  return ESP32Can.begin(ESP32Can.convertSpeed(1000), tx_pin, rx_pin, 10, 10);
+}
+
+bool CybergearCanInterfaceTwai::send_message(
+  uint32_t id, const uint8_t * data, uint8_t len, bool ext)
+{
+  CG_DEBUG_FUNC
+  CanFrame frame = {0};
+  frame.identifier = id;
+  frame.extd = (ext) ? 1 : 0;
+  frame.data_length_code = len;
+  memcpy(frame.data, data, len);
+  return ESP32Can.writeFrame(frame);
+}
+
+bool CybergearCanInterfaceTwai::read_message(unsigned long & id, uint8_t * data, uint8_t & len)
+{
+  CG_DEBUG_FUNC
+  CanFrame frame = {0};
+  if (ESP32Can.readFrame(frame, 1) == false) {
+    return false;
+  }
+
+  if (frame.rtr) return false;
+
+  id = frame.identifier;
+  len = frame.data_length_code;
+  memcpy(data, frame.data, frame.data_length_code);
+  return true;
+}
+
+bool CybergearCanInterfaceTwai::available()
+{
+  CG_DEBUG_FUNC
+  return (ESP32Can.inRxQueue() > 0);
+}
---
+++ src/cybergear_can_interface_twai.hh
@@ -0,0 +1,20 @@
+#ifndef CYBERGEAR_CAN_INTERFACE_TWAI_HH
+#define CYBERGEAR_CAN_INTERFACE_TWAI_HH
+
+#include "cybergear_can_interface.hh"
+
+#define M5_ESP32_DEFAULT_RX_PIN 1
+#define M5_ESP32_DEFAULT_TX_PIN 2
+
+class CybergearCanInterfaceTwai : public CybergearCanInterface
+{
+public:
+  CybergearCanInterfaceTwai();
+  virtual ~CybergearCanInterfaceTwai();
+  bool init(uint8_t rx_pin = M5_ESP32_DEFAULT_RX_PIN, uint8_t tx_pin = M5_ESP32_DEFAULT_TX_PIN);
+  virtual bool send_message(uint32_t id, const uint8_t * data, uint8_t len, bool ext);
+  virtual bool read_message(unsigned long & id, uint8_t * data, uint8_t & len);
+  virtual bool available();
+};
+
+#endif  // CYBERGEAR_CAN_INTERFACE_TWAI_HH
